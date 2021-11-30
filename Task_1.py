362b77d4693c2f1be133784d5e72fdf1c9574a1e
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-11-30 21:10:39 +0200

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -13,11 +13,11 @@ def user(username, password, silent=False):
     users = {'username1': 'password1', 'username2': 'password2', 'username3': 'password3',
             'username4': 'password4', 'username5': 'password5'}
     for key, value in users.items():
-        if (username == key and password == value):
+        if username == key and password == value:
             return True 
-        if (username != key and password != value and silent == True):
+        if username != key and password != value and silent == True:
             return False 
-        if (username != key and password != value and silent == False):
+        if username != key and password != value and silent == False:
             raise LoginException("Wrong login and password!")
     
 test_username = str(input("Enter username: "))
