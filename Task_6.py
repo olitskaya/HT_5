30ae0fdbed1aabd990cbb6f9a1708dc87d721f2b
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-12-01 14:31:23 +0200

    Create Task_6.py

---
+++ Task_6.py
@@ -0,0 +1,14 @@
+# Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
+# P.S. Повинен вертатись генератор.
+# P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
+
+def range_test(start, stop, step = 1):
+	list_test = []
+	list_test.append(start)
+	i = start
+	while i < stop :
+		i += step
+		list_test.append(i)
+	return list_test
+	
+print(range_test(0, 10, 2))
