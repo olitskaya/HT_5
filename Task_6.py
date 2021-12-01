f0baf9188acf7b50bc29fe16757551270c973867
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-12-01 15:52:49 +0200

    Update Task_6.py

--- Task_6.py
+++ Task_6.py
@@ -2,11 +2,7 @@
 # P.S. Повинен вертатись генератор.
 # P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 
-def range_test(start, stop, step = 1):
-	list_test = []
-	list_test.append(start)
-	i = start
-	while i < stop :
-		i += step
-		list_test.append(i)
-	yield i
+def range_test(start, stop, step=1):
+    while i > start and i < stop:
+        yield i
+        i = i + step
