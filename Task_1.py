39cfd9d3117fd231d70a74cc4f4a652f40610dd5
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-12-02 17:43:22 +0200

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -9,14 +9,17 @@
 class LoginException(Exception):
     pass
 
-def user(username, password, silent=False):
+def user(username, password, silent = False):
     users = [('username1', 'password1'), ('username2', 'password2'), ('username3', 'password3'), ('username4', 'password4'), ('username5', 'password5')]
     user_test = (username, password)
-    if users[0] == user_test or users[1] == user_test or users[2] == user_test or users[3] == user_test or users[4] == user_test:
-        return True
-    if users[0] == user_test or users[1] == user_test or users[2] == user_test or users[3] == user_test or users[4] == user_test and silent:
-        return False
-    if users[0] == user_test or users[1] == user_test or users[2] == user_test or users[3] == user_test or users[4] == user_test and silent == False:
+    for pair in users:
+        a = False
+        if pair == user_test:
+            a = True
+            return a
+    if silent:
+        return a
+    else:
         raise LoginException("Wrong login and password!")
     
 test_username = str(input("Enter username: "))
